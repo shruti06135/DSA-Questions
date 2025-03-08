d6d11fc5971cc668254f04c719d132233d50cd33
shruti06135 <shrutigarg06135@gamil.com>
2025-03-08 22:49:26 +0530

    Day7 questions upload

--- Day7.cpp
+++ Day7.cpp
@@ -7,19 +7,18 @@ using namespace std;
 class Solution {
     public:
         bool canPlaceFlowers(vector<int>& flowerbed, int n) {
-            if(n==0) return true;
+            if (n == 0) return true;
             int cnt = 0;
             int s = flowerbed.size();
     
             for (int i = 0; i < s; i++) {
-            if (flowerbed[i] == 0 && (i == 0 || flowerbed[i - 1] == 0) && (i == s - 1 || flowerbed[i + 1] == 0)) {  
-                flowerbed[i] = 1;
-                cnt++;
-    
-                if (cnt >= n) {
-                    return true;
+                if (flowerbed[i] == 0 && (i == 0 || flowerbed[i - 1] == 0) &&
+                    (i == s - 1 || flowerbed[i + 1] == 0)){
+                    flowerbed[i] = 1;
+                    cnt++;
                 }
             }
+            if (cnt >= n) return true;
+            return false;
         }
-    }
-};
+    };
